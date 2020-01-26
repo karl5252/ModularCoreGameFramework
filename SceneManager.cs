42f48ec2ea94a5828fdbe909d2b1ec657eab91df
karl5252 <32590103+karl5252@users.noreply.github.com>
2020-01-26 22:56:33 +0100

    Add files via upload

---
+++ SceneManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public string[] levelNames;
+    public int gameLevelNumber;
+    public GameObject gameManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        gameManager = GameObject.Find("GameManger");
+        if (gameManager == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadLevel(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+    private void LoadLevel(int indexNum) //alternative
+    {
+        LoadLevel(levelNames[indexNum]);
+    }
+    public void loadNextLevel()
+    {
+        if (gameLevelNumber >= levelNames.Length)
+            gameLevelNumber = 0;
+
+        LoadLevel(gameLevelNumber);
+        gameLevelNumber++;
+    }
+    public void ResetGame()
+    {
+        gameLevelNumber = 0;
+    }
+
+}
+    
