28502e159e79de33d18c512262163ae9e10f1871
karl5252 <32590103+karl5252@users.noreply.github.com>
2020-01-27 07:56:20 +0100

    Caching script for mono behaviour

---
+++ cachingMonoBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cachingMonoBehaviour : MonoBehaviour
+{
+    public Transform myTransform;
+    public GameObject myGObject;
+    public Rigidbody myRBody;
+
+    public bool didInitialized;
+    public bool canControl;
+
+    public int id;
+
+    [System.NonSerialized]
+    public Vector3 tempVBC;
+    [System.NonSerialized]
+    public Transform tempTR;
+
+    public virtual void SetId(int anId)
+    {
+
+    }
+    
+}
